4875f5df2d6707010a853902fa821942b9a0499b
Leigh Brenecki <leigh@brenecki.id.au>
2019-11-08 22:02:35 +1030

    60fps + gamma correction

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import math
 from random import randint
 import utime
 
+# from here: https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+gamma_lookup = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255]
+
 
 def hsv2rgb(h, s, v):
     h = float(h)
@@ -33,7 +51,7 @@ def hsv2rgb(h, s, v):
     return r, g, b
 
 
-FPS = 45
+FPS = 60
 US_PER_FRAME = 1000000 // FPS
 
 
@@ -74,6 +92,8 @@ def animation_frames():
         if diff > 0:
             utime.sleep_us(diff)
 
+def gamma(color):
+    return (gamma_lookup[color[0]], gamma_lookup[color[1]], gamma_lookup[color[2]])
 
 animations = [Animation(randint(0, FPS * 3)) for _ in range(0, 50)]
 
@@ -83,5 +103,5 @@ for frame_num in animation_frames():
         if a.is_expired(frame_num):
             a = Animation(frame_num)
             animations[i] = a
-        np[i] = a.get_frame(frame_num)
+        np[i] = gamma(a.get_frame(frame_num))
     np.write()
